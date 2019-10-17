6cd7d6c9731d2eaef83513e38f25a59694adc07b
warmflasha <warmflasha@gmail.com>
2019-10-17 12:23:54 -0500

    Added in class 13

---
+++ inclass13.m
@@ -0,0 +1,16 @@
+%Inclass 13. 
+
+% Continue with the set of images you used for inclass 12, the same time 
+% point (t = 30)
+
+% 1. Use the channel that marks the cell nuclei. Produce an appropriately
+% smoothed image with the background subtracted. 
+
+% 2. threshold this image to get a mask that marks the cell nuclei. 
+
+% 3. Use any morphological operations you like to improve this mask (i.e.
+% no holes in nuclei, no tiny fragments etc.)
+
+% 4. Use the mask together with the images to find the mean intensity for
+% each cell nucleus in each of the two channels. Make a plot where each data point 
+% represents one nucleus and these two values are plotted against each other
